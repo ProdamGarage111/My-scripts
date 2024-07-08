1cf3f7cda0a2abed1f5ee17fd4af28d5f48d3c3a
ProdamGarage111 <nata.simonenko.72@mail.ru>
2024-07-08 13:05:39 -0400

    Initial commit

---
+++ my_first_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This is a comment
+# Print "Hello, World!"
+echo "Hello, World!"
+
+# List files in the current directory
+ls -l
---
+++ my_second_script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+result=$(ls -l)
+echo "$result"
